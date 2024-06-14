d3c445e6147d534610e7f36e2edd798bd3389e21
Saakshi Kobarne <saakshik15@gmail.com>
2024-06-14 22:27:09 +0530

    Complete Clerk setup

---
+++ convex/auth.config.ts
@@ -0,0 +1,10 @@
+const authConfig = {
+    providers: [
+      {
+        domain: "https://working-ant-90.clerk.accounts.dev",
+        applicationID: "convex",
+      },
+    ]
+  };
+
+  export default authConfig;
